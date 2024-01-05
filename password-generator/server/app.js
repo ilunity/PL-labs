69c3e012d46c5307d233d2d374dd390857adbcbb
Ilya Shishkin <mostbook@mail.ru>
2024-01-05 18:17:29 +0700

    lab2. Server.
    Add cors middleware

--- password-generator/server/app.js
+++ password-generator/server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const passwordsRouter = require('./routes/passwords');
 
@@ -13,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
